6b02cb0952990b7dd662e35c9a9174f83113cc1d
Nicholas Tierney <nicholas.tierney@gmail.com>
2020-11-16 17:58:26 +1100

    use targets

---
+++ R/historical_section_weather.R
@@ -0,0 +1,14 @@
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##'
+##' @title
+##' @param section_stations
+##' @return
+##' @author Nicholas Tierney
+##' @export
+historical_section_weather <- function(section_stations) {
+
+  NULL
+
+}
